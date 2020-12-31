ecd426dd73a6360ae60027ff1abf92d2ffcce0a2
Waqas Khan <wuikhan@gmail.com>
2020-12-30 19:58:01 -0500

    sal-4 verify account name field is added to account object

--- src/test/java/stepDefinition/DashboardTest.java
+++ src/test/java/stepDefinition/DashboardTest.java
@@ -1,10 +1,12 @@
 package stepDefinition;
 
 import org.openqa.selenium.By;
+import org.testng.Assert;
 
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+
 import utilities.BaseClass;
 
 public class DashboardTest extends BaseClass {
@@ -33,7 +35,7 @@ public class DashboardTest extends BaseClass {
 
 	@When("^I click the \"([^\"]*)\" button$")
 	public void i_click_the_button(String btn) {
-	   driver.findElement(By.xpath("//input[@id='"+btn+"']")).click();
+	   driver.findElement(By.xpath("//input[@name='"+btn+"']")).click();
 	}
 
 	@Then("^I should see the \"([^\"]*)\" link$")
@@ -44,5 +46,24 @@ public class DashboardTest extends BaseClass {
 	@Then("^I quit the browser$")
 	public void i_quit_the_browser()  {
 	   driver.quit();
+	}
+	
+	@Then("^I click the \"([^\"]*)\" tab$")
+	public void i_click_the_tab(String tabLink)  {
+	    driver.findElement(By.linkText(tabLink)).click();
+	}
+
+	@Then("^I should see the text \"([^\"]*)\"$")
+	public void i_should_see_the_text(String arg1)  {
+	   String actualText = "Review all error messages below to correct your data.";
+	  String expectedText = driver.findElement(By.id("errorDiv_ep")).getText();
+	  if(expectedText.contains(actualText)) {
+		  Assert.assertTrue(true);
+	  }
+	}
+
+	@Then("^I enter \"([^\"]*)\" text for \"([^\"]*)\" field$")
+	public void i_enter_text_for_field(String text, String elementId) {
+	    driver.findElement(By.id(elementId)).sendKeys(text);
 	}
 }
